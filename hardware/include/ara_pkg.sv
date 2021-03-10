f076e6a342dec4456ee728ea22c40129fcbd6acd
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-03-10 14:40:55 +0100

    [hardware] Add support for FP widening type-convert instructions

--- hardware/include/ara_pkg.sv
+++ hardware/include/ara_pkg.sv
@@ -97,7 +97,7 @@ package ara_pkg;
     VDIVU, VDIV, VREMU, VREM,
     // FPU
     VFADD, VFSUB, VFRSUB, VFMUL, VFMACC, VFNMACC, VFMSAC, VFNMSAC, VFMADD, VFNMADD, VFMSUB, VFNMSUB, VFMIN, VFMAX, VFSGNJ, VFSGNJN, VFSGNJX,
-    VFCVTXUF, VFCVTXF, VFCVTFXU, VFCVTFX, VFCVTRTZXUF, VFCVTRTZXF,
+    VFCVTXUF, VFCVTXF, VFCVTFXU, VFCVTFX, VFCVTRTZXUF, VFCVTRTZXF, VFCVTFF,
     // Mask operations
     VMANDNOT, VMAND, VMOR, VMXOR, VMORNOT, VMNAND, VMNOR, VMXNOR,
     // Integer comparison instructions
@@ -146,6 +146,15 @@ package ara_pkg;
     OpQueueConversionWideFP2,
     OpQueueAdjustFPCvt
   } opqueue_conversion_e;
+  // OpQueueAdjustFPCvt is introduced to support widening FP conversions, to comply with the
+  // required SIMD input format of the FPU module (fpnew)
+
+  // The FPU needs to know if, during the conversion, there is also a width change
+  typedef enum logic [1:0] {
+    CVT_SAME,
+    CVT_WIDE,
+    CVT_NARROW
+  } fp_resize_e;
 
   // Floating-Point structs for re-encoding during widening FP operations
   typedef struct packed {
@@ -224,6 +233,8 @@ package ara_pkg;
     fpnew_pkg::roundmode_e fp_rm;
     // Widen FP immediate (re-encoding)
     logic wide_fp_imm;
+    // Resizing of FP conversions
+    fp_resize_e fp_cvt_resize;
 
     // Vector machine metadata
     vlen_t vl;
@@ -310,6 +321,8 @@ package ara_pkg;
     fpnew_pkg::roundmode_e fp_rm;
     // Widen FP immediate (re-encoding)
     logic wide_fp_imm;
+    // Resizing of FP conversions
+    fp_resize_e fp_cvt_resize;
 
     // Vector machine metadata
     vlen_t vl;
@@ -549,6 +562,7 @@ package ara_pkg;
 
     fpnew_pkg::roundmode_e fp_rm; // Rounding-Mode for FP operations
     logic wide_fp_imm;            // Widen FP immediate (re-encoding)
+    fp_resize_e fp_cvt_resize; // Resizing of FP conversions
 
     // Vector machine metadata
     vlen_t vl;
